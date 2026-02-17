210fff67d442dec8f3e7a58a71459b2837749c94
anantinfo <95564262+anantinfo@users.noreply.github.com>
2026-02-17 11:11:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -84,3 +84,13 @@ behavior:"smooth"
 });
 
 };
+
+
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+
